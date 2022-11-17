e0e7efe234a9bcafb4086ea4421f1d1dc470e6b3
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-11-17 20:29:45 +0600

    forget password responsive done

--- src/Pages/ForgetPassword/ForgetPassword.js
+++ src/Pages/ForgetPassword/ForgetPassword.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './ForgetPassword.css'
 
 const ForgetPassword = () => {
     return (
-        <div className='container mx-auto w-50 py-3 px-5'>
+        <div className='container forget-width mx-auto py-3 px-5'>
             <p>
                 Lost your password? Please enter your username or email address. <br /> You will receive a link to create a new password via email. <br />
 
